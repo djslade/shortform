ac1ba4305aadfa32d2d186914a48b781716f706a
fenroe <scrambles9494@gmail.com>
2025-02-07 11:22:01 +0200

    finished footer desktop

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -38,20 +38,28 @@ const footerLinksData = [
 export const Footer = () => {
   return (
     <footer className="footer">
-      <img
-        src="/text-logo-footer.svg"
-        alt="Shortform"
-        className="footer-logo"
-      />
-      <>
-        {footerLinksData.map((fl) => (
-          <FooterLinks key={fl.heading} heading={fl.heading} links={fl.links} />
-        ))}
-      </>
-      <div className="footer-social-container">
-        <a href="/" className="footer-social">
-          <FaGithub className="footer-social-icon" />
-        </a>
+      <div className="footer-inner-container">
+        <div className="footer-left">
+          <img
+            src="/text-logo-footer.svg"
+            alt="Shortform"
+            className="footer-logo"
+          />
+        </div>
+        <div className="footer-right">
+          {footerLinksData.map((fl) => (
+            <FooterLinks
+              key={fl.heading}
+              heading={fl.heading}
+              links={fl.links}
+            />
+          ))}
+          <div className="footer-social-container">
+            <a href="/" className="footer-social">
+              <FaGithub className="footer-social-icon" />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
