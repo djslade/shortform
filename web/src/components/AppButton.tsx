1d08afb52bc1dfcdd2b0119b3f3377433f4d45be
fenroe <scrambles9494@gmail.com>
2025-02-11 23:53:21 +0200

    add variant to appbutton

--- web/src/components/AppButton.tsx
+++ web/src/components/AppButton.tsx
@@ -3,11 +3,29 @@ import "../styles/AppButton.css";
 interface AppButtonProps {
   text: string;
   type?: "button" | "submit" | "reset";
+  variant?: "small";
   tinted?: boolean;
   onClick?: () => void;
 }
 
-export const AppButton = ({ text, type, tinted, onClick }: AppButtonProps) => {
+export const AppButton = ({
+  text,
+  type,
+  variant,
+  tinted,
+  onClick,
+}: AppButtonProps) => {
+  if (variant == "small") {
+    return (
+      <button
+        className={`app-btn-small ${tinted ? "app-btn-tint" : ""}`}
+        type={type}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  }
   return (
     <button
       className={`app-btn ${tinted ? "app-btn-tint" : ""}`}
